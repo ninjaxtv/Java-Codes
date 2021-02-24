e7dfa08b67ef76fc0b548df799c0b514c7f34b4c
ninjaxtv <77315008+ninjaxtv@users.noreply.github.com>
2021-02-24 19:01:36 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboardInput = new Scanner(System.in);
+
+        System.out.print("Enter color: ");
+        String color = keyboardInput.nextLine();
+
+        System.out.print("Enter plural noun: ");
+        String pluralNoun = keyboardInput.nextLine();
+
+        System.out.print("Enter celebrity: ");
+        String celebrity = keyboardInput.nextLine();
+
+        System.out.println("Roses are " + color);
+        System.out.println(pluralNoun + " are blue");
+        System.out.println("I love " + celebrity);
+    }
+}
